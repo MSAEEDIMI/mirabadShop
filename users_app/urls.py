189965b00a5958448768e41b7961311dd901179a
MSAEEDIMI <m1569753@gmail.com>
2025-07-17 00:51:21 +0330

    convert model def veiw to class base veiw for users_app veiws and singin and log in and logout finished  and handeld some erors for registr form and login form

--- users_app/urls.py
+++ users_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name='users_app'
 
 urlpatterns = [
-    path('login/',views.loginToSite,name='login'),
+    path('login/',views.LoginFormView.as_view(),name='login'),
     path('logout/',views.logoutFromSite,name='logout'),
-    path('register/',views.register,name='register'),
+    path('register/',views.RegisterFormView.as_view(),name='register'),
 ]
--- users_app/views.py
+++ users_app/views.py
@@ -1,56 +1,80 @@
 from django.contrib.auth import logout ,authenticate,login
+from django.http import HttpResponse
 from django.shortcuts import render ,redirect
 from .forms import Regestrform,LoginForm
 from django.contrib.auth.models import User
-
-
-def register(request):
-    if request.user.is_authenticated:
-        return redirect('home_app:home')
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+# def register(request):
+#     if request.user.is_authenticated:
+#         return redirect('home_app:home')
     
-    if request.method == "POST":
-        form = Regestrform(request.POST)
-        context={"errors":[],
-                 "form":form,
-                 }
-        if form.is_valid():
-            username=form.cleaned_data.get("username")
-            first_name=form.cleaned_data.get("first_name")
-            last_name=form.cleaned_data.get("last_name")
-            email=form.cleaned_data.get("email")
-            phone_number=form.cleaned_data.get("phone_number")
-            password1=form.cleaned_data.get("password1")
-            password2=form.cleaned_data.get("password2")
-            if password1 !=password2:
-                context["errors"].append("پسورد ها مشابه نیستند")
-                return render(request,"users_app/register.html",context)
-            if User.objects.filter(username=username).exists():
-                context["errors"].append("کاربر با این نام کاربری موجود است!!! ")
-                return render(request,"users_app/register.html",context)
-            user=User.objects.create_user(username=username,first_name=first_name,last_name=last_name,password=password2,email=email) # type: ignore
-            login(request,user)    
-            return redirect("home_app:home")
+#     if request.method == "POST":
+#         form = Regestrform(request.POST)
+#         context={"errors":[],
+#                  "form":form,
+#                  }
+#         if form.is_valid():
+#             username=form.cleaned_data.get("username")
+#             first_name=form.cleaned_data.get("first_name")
+#             last_name=form.cleaned_data.get("last_name")
+#             email=form.cleaned_data.get("email")
+#             phone_number=form.cleaned_data.get("phone_number")
+#             password1=form.cleaned_data.get("password1")
+#             password2=form.cleaned_data.get("password2")
+#             if password1 !=password2:
+#                 context["errors"].append("پسورد ها مشابه نیستند")
+#                 return render(request,"users_app/register.html",context)
+#             if User.objects.filter(username=username).exists():
+#                 context["errors"].append("کاربر با این نام کاربری موجود است!!! ")
+#                 return render(request,"users_app/register.html",context)
+#             user=User.objects.create_user(username=username,first_name=first_name,last_name=last_name,password=password2,email=email) # type: ignore
+#             login(request,user)    
+#             return redirect("home_app:home")
+#         else:
+#             context["errors"].append("اطلاعات وارد شده معتبر نیست.")
+#             return render(request, "users_app/register.html", context)
+#     else:
+#         form = Regestrform()
+#     return render(request,"users_app/register.html",{"form":form})
+
+class RegisterFormView(FormView):
+    template_name = "users_app/register.html"
+    form_class=Regestrform
+    success_url=reverse_lazy("home_app:home")
+    
+    def form_valid(self, form):
+        username=form.cleaned_data.get("username")
+        first_name=form.cleaned_data.get("first_name")
+        last_name=form.cleaned_data.get("last_name")
+        email=form.cleaned_data.get("email")
+        phone_number=form.cleaned_data.get("phone_number")
+        password1=form.cleaned_data.get("password1")
+        password2=form.cleaned_data.get("password2")
+        user=User.objects.create_user(username=username,first_name=first_name,last_name=last_name,password=password2,email=email) # type: ignore
+        login(self.request,user)    
+        return super().form_valid(form)
+    
+    
+    
+
+
+class LoginFormView(FormView):
+    template_name = "users_app/login.html"
+    form_class = LoginForm
+    success_url = reverse_lazy("home_app:home")
+
+    def form_valid(self, form):
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(self.request, username=username, password=password)
+        if user is not None:
+            login(self.request, user)
+            return super().form_valid(form)
         else:
-            context["errors"].append("اطلاعات وارد شده معتبر نیست.")
-            return render(request, "users_app/register.html", context)
-    else:
-        form = Regestrform()
-    return render(request,"users_app/register.html",{"form":form})
-
-
-def loginToSite(requst):
-    if requst.method=="POST":
-        form=LoginForm(requst.POST)
-        if form.is_valid:
-            username=form.cleaned_data.get('username')
-            user=User.objects.get(username=username)
-            login(requst,user)
-            return redirect("home_app:home")
-    else:
-        form=LoginForm()
+            form.add_error(None, "نام کاربری یا رمز عبور اشتباه است.")
+            return self.form_invalid(form)
     
-    return render(requst,"users_app/login.html",context={"form":form})
-
 
 
 def logoutFromSite(requst):
