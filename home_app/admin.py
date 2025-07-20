0b93694a540cca40a5af313af351bcf312a19da0
MSAEEDIMI <m1569753@gmail.com>
2025-07-20 20:01:37 +0330

    complit home page  admin

--- home_app/admin.py
+++ home_app/admin.py
@@ -4,4 +4,9 @@ from .models import SiteInfo ,Banner
 # Register your models here.
 # Register your models here.
 admin.site.register(SiteInfo)
-admin.site.register(Banner)
+
+@admin.register(Banner)
+class BanerAdmin(admin.ModelAdmin):
+    list_display=['position','order']
+    list_filter=['position']
+    
--- home_app/models.py
+++ home_app/models.py
@@ -2,33 +2,38 @@ from django.db import models
 
 # Create your models here.
 class SiteInfo(models.Model):
-    title=models.CharField(max_length=50)
-    phone=models.CharField(max_length=11)
-    instagram=models.URLField(max_length=200,null=True)
-    rubika=models.URLField(max_length=200,null=True)
-    telegram=models.URLField(max_length=200,null=True)
-    discreption=models.TextField()
-    logo=models.ImageField(upload_to="logo")
-    name1=models.CharField(max_length=50)
-    perseneImage1=models.ImageField(upload_to="modiriat")
-    name2=models.CharField(max_length=50)
-    perseneImage2=models.ImageField(upload_to="modiriat")
-    name3=models.CharField(max_length=50)
-    perseneImage3=models.ImageField(upload_to="modiriat")
+    title=models.CharField(max_length=50,verbose_name='عنوان سایت')
+    phone=models.CharField(max_length=11,verbose_name='شماره پشتیبانی')
+    instagram=models.URLField(max_length=200,null=True,verbose_name='ایدی اینستاگرام')
+    rubika=models.URLField(max_length=200,null=True,verbose_name='ایدی روبیکا')
+    telegram=models.URLField(max_length=200,null=True,verbose_name='ایدی تلگرام')
+    discreption=models.TextField(verbose_name='توضیحات سایت')
+    logo=models.ImageField(upload_to="logo",verbose_name='لگوی سایت')
+    name1=models.CharField(max_length=50,verbose_name='نام پرسنل ۱')
+    perseneImage1=models.ImageField(upload_to="modiriat",verbose_name='عکس پرسنل ۱')
+    name2=models.CharField(max_length=50,verbose_name='نام پرسنل۲')
+    perseneImage2=models.ImageField(upload_to="modiriat",verbose_name='عکس پرسنل۲')
+    name3=models.CharField(max_length=50,verbose_name='نام پرسنل۳')
+    perseneImage3=models.ImageField(upload_to="modiriat",verbose_name='عکس پرسنل۳')
     def __str__(self):
         return f"titel of the site is ({self.title})"
-
+    class Meta:
+        verbose_name=' اطلاعات سایت و پرسنل'
+        verbose_name_plural=' اطلاعات سایت و پرسنل'
 
 class Banner(models.Model):
-    title = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(upload_to='banners/')
-    link = models.URLField(blank=True, null=True)
+    title = models.CharField(max_length=200, blank=True,verbose_name='عنوان')
+    image = models.ImageField(upload_to='banners/',verbose_name='عکس')
+    link = models.URLField(blank=True, null=True,verbose_name='لینک ')
     position = models.CharField(max_length=50, choices=[
-        ('main', 'بنر اصلی'),
-        ('sidebar', 'سایدبار'),
+        ('main', 'اسلایدر اصلی'),
+        ('sidebar', 'بنر کنار اسلایدر'),
         ('footer', 'بنر پایین صفحه'),
-    ])
-    order = models.PositiveIntegerField(default=0)
+    ],verbose_name='جایگاه')
+    order = models.PositiveIntegerField(default=0,verbose_name='اولویت نمایش')
 
     def __str__(self):
         return self.position
+    class Meta:
+        verbose_name=' اسلایدر و بنرها'
+        verbose_name_plural='اسلایدر و بنر '
