358b772b8172513280b056d58ac97021bbfe3022
MSAEEDIMI <m1569753@gmail.com>
2025-07-20 18:55:55 +0330

    complit product admin page and change font

--- products_app/admin.py
+++ products_app/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Product, Category
-admin.site.register(Product)
 admin.site.register(Category)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=["name","base_price","final_price",'category',"discount",'stock_quantity']
+    list_filter=['category',]
+    list_editable=["discount","base_price"]
+    search_fields=["name"]
--- products_app/models.py
+++ products_app/models.py
@@ -4,13 +4,15 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 class Category(models.Model):
-    name = models.CharField(max_length=30)
-    slug=models.SlugField()
-    all_discount = models.IntegerField(default=0)
+    name = models.CharField(max_length=30,verbose_name='دسته بندی ')
+    slug=models.SlugField(verbose_name='دسته بندی به انگلیسی')
+    all_discount = models.IntegerField(default=0,verbose_name='تخفیف برای تمام محصولات این دسته ')
 
     def __str__(self):
         return self.name
-
+    class Meta:
+        verbose_name=' دسته بندی '
+        verbose_name_plural='دسته بندی ها'
 
 UNIT_CHOICES = [
     ('kg', 'Kilogram'),   # کیلویی
@@ -19,22 +21,22 @@ UNIT_CHOICES = [
 ]
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    name = models.CharField(max_length=40)
-    name_to_english = models.CharField(max_length=50)
-    base_price = models.IntegerField(help_text="قیمت پایه بدون تخفیف")
+    category = models.ForeignKey(Category, on_delete=models.PROTECT,verbose_name='دسته')
+    name = models.CharField(max_length=40,verbose_name='نام محصول')
+    name_to_english = models.CharField(max_length=50,verbose_name='نام انگلیسی محصول')
+    base_price = models.IntegerField(help_text="قیمت پایه بدون تخفیف",verbose_name='قیمت پایه ')
 
-    discount = models.IntegerField(default=0, help_text="درصد تخفیف محصول")
-    image = models.ImageField(upload_to="products_image", blank=True, null=True)
+    discount = models.IntegerField(default=0, help_text="درصد تخفیف محصول",verbose_name='تخفیف برای این محصول')
+    image = models.ImageField(upload_to="products_image", blank=True, null=True,verbose_name='عکس محصول')
     
-    unit = models.CharField(max_length=10, choices=UNIT_CHOICES, default='kg')
-    unit_amount = models.DecimalField(max_digits=5, decimal_places=2, default=1, help_text="مقدار واحد (مثلاً 0.5 برای بسته نیم کیلو)") # type: ignore
+    unit = models.CharField(max_length=10, choices=UNIT_CHOICES, default='kg',verbose_name='واحد')
+    unit_amount = models.DecimalField(max_digits=5, decimal_places=2, default=1, help_text="مقدار واحد (مثلاً 0.5 برای بسته نیم کیلو)",verbose_name='مقدار وزن هر بسته') # type: ignore
 
-    stock_quantity = models.DecimalField(max_digits=10, decimal_places=3, default=0, help_text="موجودی انبار") # type: ignore
-    sales_count = models.PositiveIntegerField(default=0, help_text="تعداد فروش")
+    stock_quantity = models.DecimalField(max_digits=10, decimal_places=0, default=0, help_text="موجودی انبار",verbose_name='موجودی انبار') # type: ignore
+    sales_count = models.PositiveIntegerField(default=0, help_text="تعداد فروش",verbose_name='مقدار فروش رفته')
 
-    likes = models.PositiveIntegerField(default=0, help_text="حداکثر 30 لایک")
-    selected = models.BooleanField(default=True)
+    likes = models.PositiveIntegerField(default=0, help_text="حداکثر 30 لایک",verbose_name='تعداد لایک ها')
+    selected = models.BooleanField(default=True,verbose_name='انتخاب شده')
     
     slug = models.SlugField(blank=True, null=True, unique=True)
 
@@ -68,3 +70,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name=' محصول'
+        verbose_name_plural='محصولات'
